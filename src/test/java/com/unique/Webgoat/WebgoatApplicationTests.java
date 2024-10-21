523b6f6084b7eee2b5ef4b1d9b131ba3670432f0
Unique <Unique@DESKTOP-0E9CIPE>
2024-10-21 03:50:39 -0700

    First Commit of Webgoat

---
+++ src/test/java/com/unique/Webgoat/WebgoatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unique.Webgoat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebgoatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
